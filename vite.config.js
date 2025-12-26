a163fad8d6fb278c902e0681b9626360594bc1cf
pochengfan <fanpocheng2006@gmail.com>
2025-12-27 03:01:28 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,3 +4,8 @@ export default defineConfig({
   base: '/eric-personal-website/',
   plugins: [react()]
 })
+npm i -D @vitejs/plugin-react
+npm run build
+git add package.json package-lock.json
+git commit -m "Add @vitejs/plugin-react"
+git push
