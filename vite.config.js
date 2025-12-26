378c9773296ab3f7ac692e547d634d90c59b44fe
pochengfan <fanpocheng2006@gmail.com>
2025-12-27 02:51:52 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// IMPORTANT: 如果你改 repo 名稱，請同步更新 base
 export default defineConfig({
   base: '/eric-personal-website/',
   plugins: [react()]
